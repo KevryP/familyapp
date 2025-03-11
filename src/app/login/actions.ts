e6d1c9f7752563bf757f2d8acb0f8f222a33b312
Kevry Paulino <kevryp@gmail.com>
2025-03-11 17:51:00 -0400

    supababase auth

---
+++ src/app/login/actions.ts
@@ -0,0 +1,54 @@
+'use server'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+import { createClient } from '../../utils/supabase/server'
+
+export async function login(formData: FormData) {
+  const supabase = await createClient()
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+  
+
+  const { error } = await supabase.auth.signInWithPassword(data)
+
+  if (error) {
+    console.log(error)
+    redirect('/error')
+  }
+  revalidatePath('/', 'layout')
+  redirect('/account')
+}
+
+export async function signup(formData: FormData) {
+  const supabase = await createClient()
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+    options: {
+      data: {
+        first_name: formData.get('firstName') as string,
+        middle_name: formData.get('middleName') as string,
+        last_name: formData.get('lastName') as string,
+      }
+    }
+  }
+  console.log(data);
+
+  const { error } = await supabase.auth.signUp(data)
+
+  if (error) {
+    redirect('/error')
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/account')
+}
